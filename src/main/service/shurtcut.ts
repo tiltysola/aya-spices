4dcc3e2e91ef48dafbe9b650cc0a694fa5ecffef
Zero <kanade@acgme.cn>
2022-08-20 17:13:58 +0800

    feat: update

--- src/main/service/shurtcut.ts
+++ src/main/service/shurtcut.ts
@@ -1,18 +1,38 @@
 import { globalShortcut } from 'electron';
+import childProcess from 'child_process'
 
+import path from 'path';
 import logger from '../utils/logger';
 import { hideWindow, showWindow, win } from '../windows/main';
 
+const foregroundWindowPath = path.join(process.cwd(), './resources/ForegroundWindow/ForegroundWindow.exe')
+
+let hWnd: string | null = null;
+
 export const registerShortcutKey = () => {
-  const accelerator = 'Control+Meta+F12';
+  const accelerator = 'Alt+1';
 
   globalShortcut.register(accelerator, () => {
     logger.debug('[Suspension]', 'Shortcut key pressed.');
     if (win && !win.isDestroyed()) {
       if (win.isVisible()) {
         hideWindow();
+        if (hWnd) {
+          childProcess.spawn(foregroundWindowPath, [
+            '--set',
+            hWnd
+          ])
+        }
       } else {
-        showWindow();
+        const cp = childProcess.spawn(foregroundWindowPath, [
+          '--get',
+        ]);
+        cp.stdout.on('data', (data) => {
+          hWnd = Buffer.from(data).toString()
+        })
+        cp.on('exit', () => {
+          showWindow();
+        })
       }
     } else {
       showWindow();
